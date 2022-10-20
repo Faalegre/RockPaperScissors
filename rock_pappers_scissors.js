ec921ccc30f15ac86edd008a4b0c361f5ac08569
Faalegre <federico.alegree@outlook.com>
2022-10-19 23:43:38 -0300

    add css and javascript to make the game more interactive, also add more divs in html for results and game displays. The images added are for the buttons and the game display to show what the user and the computer choises.

--- rock_pappers_scissors.js
+++ rock_pappers_scissors.js
@@ -1,6 +1,8 @@
-const buttonRock = document.getElementById("rockBtn")
-const buttonPapper = document.getElementById("papperBtn")
-const buttonScissor = document.getElementById("scissorBtn")
+const buttonRock = document.getElementById("rockBtn");
+const buttonPapper = document.getElementById("papperBtn");
+const buttonScissor = document.getElementById("scissorBtn");
+const resultCont = document.getElementById("resultContainer");
+const gameContainer = document.getElementById("game-container");
 
 buttonRock.addEventListener("click", playRound);
 buttonPapper.addEventListener("click", playRound);
@@ -15,22 +17,16 @@ let tieScore = 0
 function rockChoice(){
     playerSelection = "rocks".toUpperCase();
     computerSelection = getComputerChoice().toUpperCase();
-    console.log(playerSelection)
-    console.log(computerSelection)
 }
 
 function papperChoice(){
     playerSelection = "pappers".toUpperCase();
-    computerSelection = getComputerChoice().toUpperCase();
-    console.log(playerSelection)
-    console.log(computerSelection)
+    computerSelection = getComputerChoice().toUpperCase()
 }
 
 function scissorChoice(){
     playerSelection = "scissors".toUpperCase();
     computerSelection = getComputerChoice().toUpperCase();
-    console.log(playerSelection)
-    console.log(computerSelection)
 }
 
 
@@ -54,7 +50,7 @@ function playRound(){
     } if (this.id ==="scissorBtn"){
         scissorChoice();
     }
-    
+    showSelection(playerSelection, computerSelection);
     globalScores(playerSelection,computerSelection);
 }
 
@@ -66,16 +62,37 @@ function globalScores(playerSelection, computerSelection){
     (playerSelection === "PAPPERS" && computerSelection === "ROCKS")){
         scorePlayer++
     } else scoreComputer++;
+    resultCont.innerHTML = "Score player = " + scorePlayer + "<br/><br/>" +
+    "Score computer = " + scoreComputer + "<br/><br/>" +
+    "Tie = " + tieScore;
     getWinner(scorePlayer, scoreComputer, tieScore)
 }
 
 function getWinner(scorePlayer, scoreComputer, tieScore){
     if (scorePlayer === 5){
-        alert("You win! " + scorePlayer + " to " + scoreComputer + " and the tie score is " + tieScore)
+        resultCont.innerHTML = "You win! " + "<br/><br/>" + "Your score is = " + scorePlayer + "<br/><br/>" 
+        + "The computer score is = " + scoreComputer + "<br/><br/>" + "The amount of ties is " + tieScore;
     }
     if (scoreComputer === 5){
-        alert("Computer wins " + scoreComputer + " to " + scorePlayer + " and the tie score is " + tieScore)
+        resultCont.innerHTML = "Computer wins " + "<br/><br/>" + "Your score is = " + scorePlayer + "<br/><br/>" 
+        + "The computer score is = " + scoreComputer + "<br/><br/>" + "The amount of ties is " + tieScore;
     } 
+}
+
+function showSelection(playerSelection, computerSelection){
+    if (playerSelection === "ROCKS"){
+        playerSelection = '<img src="./images/rock.svg" style="width:200px; height:200px;" />'
+    } else if (playerSelection === "PAPPERS"){
+        playerSelection = '<img src="./images/papper.svg" style="width:200px; height:200px;" />'
+    } else playerSelection = '<img src="./images/scissor.svg" style="width:200px; height:200px;" />'
+
+    if (computerSelection === "ROCKS"){
+        computerSelection = '<img src="./images/rock.svg" style="width:200px; height:200px;" />'
+    } else if (computerSelection === "PAPPERS"){
+        computerSelection = '<img src="./images/papper.svg" style="width:200px; height:200px;" />'
+    } else computerSelection = '<img src="./images/scissor.svg" style="width:200px; height:200px;" />'
+
+    gameContainer.innerHTML = playerSelection + " VS " + computerSelection
 }
 
 
