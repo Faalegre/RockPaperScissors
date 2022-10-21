e3ce4ffdc5090aa681ac5dc7699c3830fcfb4e9f
Faalegre <federico.alegree@outlook.com>
2022-10-21 04:40:38 -0300

    add header and footer, add a Reset function and modified some style. Also clean the code

--- rock_pappers_scissors.js
+++ rock_pappers_scissors.js
@@ -3,15 +3,18 @@ const buttonPapper = document.getElementById("papperBtn");
 const buttonScissor = document.getElementById("scissorBtn");
 const resultCont = document.getElementById("resultContainer");
 const gameContainer = document.getElementById("game-container");
+const buttonCont = document.getElementById("button-container");
+
 
 buttonRock.addEventListener("click", playRound);
 buttonPapper.addEventListener("click", playRound);
 buttonScissor.addEventListener("click", playRound);
 
 
-let scorePlayer = 0
-let scoreComputer = 0
-let tieScore = 0
+
+let scorePlayer = 0;
+let scoreComputer = 0;
+let tieScore = 0;
 
 
 function rockChoice(){
@@ -19,10 +22,12 @@ function rockChoice(){
     computerSelection = getComputerChoice().toUpperCase();
 }
 
+
 function papperChoice(){
     playerSelection = "pappers".toUpperCase();
-    computerSelection = getComputerChoice().toUpperCase()
+    computerSelection = getComputerChoice().toUpperCase();
 }
+
 
 function scissorChoice(){
     playerSelection = "scissors".toUpperCase();
@@ -54,46 +59,74 @@ function playRound(){
     globalScores(playerSelection,computerSelection);
 }
 
+
 function globalScores(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        tieScore++
+        tieScore++;
     } else if ((playerSelection === "ROCKS" && computerSelection === "SCISSORS") ||
     (playerSelection === "SCISSORS" && computerSelection === "PAPPERS") ||
     (playerSelection === "PAPPERS" && computerSelection === "ROCKS")){
-        scorePlayer++
+        scorePlayer++;
     } else scoreComputer++;
-    resultCont.innerHTML = "Score player = " + scorePlayer + "<br/><br/>" +
-    "Score computer = " + scoreComputer + "<br/><br/>" +
+    resultCont.innerHTML = "Score player = " + scorePlayer + "<br></br>" +
+    "Score computer = " + scoreComputer + "<br></br>" +
     "Tie = " + tieScore;
-    getWinner(scorePlayer, scoreComputer, tieScore)
+    getWinner(scorePlayer, scoreComputer, tieScore);
+    resetGame();
 }
+
 
 function getWinner(scorePlayer, scoreComputer, tieScore){
     if (scorePlayer === 5){
-        resultCont.innerHTML = "You win! " + "<br/><br/>" + "Your score is = " + scorePlayer + "<br/><br/>" 
-        + "The computer score is = " + scoreComputer + "<br/><br/>" + "The amount of ties is = " + tieScore;
+        resultCont.innerHTML = "Your score is = " + scorePlayer + "<br></br>" + 
+        "The computer score is = " + scoreComputer + "<br></br>" + 
+        "The amount of ties is = " + tieScore;
     }
     if (scoreComputer === 5){
-        resultCont.innerHTML = "Computer wins! " + "<br/><br/>" + "Your score is = " + scorePlayer + "<br/><br/>" 
-        + "The computer score is = " + scoreComputer + "<br/><br/>" + "The amount of ties is = " + tieScore;
-    } 
+        resultCont.innerHTML = "Your score is = " + scorePlayer + "<br></br>" + 
+        "The computer score is = " + scoreComputer + "<br></br>" + 
+        "The amount of ties is = " + tieScore;
+    }
 }
+
 
 function showSelection(playerSelection, computerSelection){
     if (playerSelection === "ROCKS"){
-        playerSelection = '<img src="./images/rock.svg" style="width:200px; height:200px;" />'
+        playerSelection = '<img src="./images/rock.svg" style="width:200px; height:200px;" />';
     } else if (playerSelection === "PAPPERS"){
-        playerSelection = '<img src="./images/papper.svg" style="width:200px; height:200px;" />'
-    } else playerSelection = '<img src="./images/scissor.svg" style="width:200px; height:200px;" />'
+        playerSelection = '<img src="./images/papper.svg" style="width:200px; height:200px;" />';
+    } else playerSelection = '<img src="./images/scissor.svg" style="width:200px; height:200px;" />';
 
     if (computerSelection === "ROCKS"){
-        computerSelection = '<img src="./images/rock.svg" style="width:200px; height:200px;" />'
+        computerSelection = '<img src="./images/rock.svg" style="width:200px; height:200px;" />';
     } else if (computerSelection === "PAPPERS"){
-        computerSelection = '<img src="./images/papper.svg" style="width:200px; height:200px;" />'
-    } else computerSelection = '<img src="./images/scissor.svg" style="width:200px; height:200px;" />'
+        computerSelection = '<img src="./images/papper.svg" style="width:200px; height:200px;" />';
+    } else computerSelection = '<img src="./images/scissor.svg" style="width:200px; height:200px;" />';
 
-    gameContainer.innerHTML = playerSelection + " VS " + computerSelection
+    gameContainer.innerHTML = `<div class="gameContainer">${playerSelection}</div>` + " VS " + 
+    `<div class="gameContainer">${computerSelection}</div>`;
 }
+
+
+function resetGame(){
+    if (scorePlayer === 5){
+        buttonCont.setAttribute('id', 'resetBtn');
+        buttonCont.innerHTML = "Game is over!" + "<br></br>" + 
+        "You win!" + "<br></br>" + 
+        "Do you wanna restart?" + "<br></br>" + "<br></br>" + 
+        `<a href="index.html" class="link">Restart</a>`;
+    }
+
+    if (scoreComputer === 5){
+        buttonCont.setAttribute('id', 'resetBtn');
+        buttonCont.innerHTML = "Game is over!" + "<br></br>" +
+        "Computer wins!" + "<br></br>" + 
+        "Do you wanna restart?" + 
+        "<br></br>" + "<br></br>" + 
+        `<a href="index.html" class="link">Restart</a>`;
+    }
+}
+
 
 
 
